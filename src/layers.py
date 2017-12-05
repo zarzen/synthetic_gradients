ac31f598a2148a0aa493198b7fc1a9e4676a7f5a
Zhen Zhang <zhangzhen.dev@gmail.com>
2017-12-05 17:39:55 -0500

    complete forward path; change name delta to partial delta
    because delta for layer(l) needs weighted sums of layer(l)
    while layer(l+1) cannot maintain inputs of layer(l) in our case

--- src/layers.py
+++ src/layers.py
@@ -10,22 +10,47 @@ import neural_nets_pb2 as nn_pb
 import neural_nets_pb2_grpc as nn_pb_grpc
 
 
-class Layer():
+class Layer(nn_pb_grpc.LayerDataExchangeServicer):
   """
   abstract layer extract common methods
   """
-  def __init__(self, upper_layer, lower_layer):
+  def __init__(self, layer_name, upper_layer, lower_layer,
+               lower_layer_nodes, current_layer_nodes,
+               nonlin, nonlin_prime):
     """
     datasets : the path of mnist dataset
+    nonlin: activation function
+    nonlin_prime: the derivative of activation function
     """
+    self.layer_name = layer_name
     self.upper_layer_addr = upper_layer
     self.lower_layer_addr = lower_layer
+    self.nonlin = nonlin
+    self.nonlin_prime = nonlin_prime
 
     # lazy initialization
     self.upper_layer_stub = None
     self.lower_layer_stub = None
 
-  def forward_upper(self, batch_id, forward_matrix, forward_labels, istrain):
+    # weights dimension
+    self.weights_shape = (current_layer_nodes, lower_layer_nodes)
+    self.weights = None
+
+    # record outputs from lower layer
+    # use batch id as key
+    # Purposes:
+    # 1) used for computing the weighted sum of current layer
+    # 2) used for computing the gradients for updating weights of current layer
+    self.lower_layer_outputs = {}
+
+    # computed from lower layer outputs for cache purpose
+    # cache for computing delta for current layer
+    # delta = partial_delta_rec * nonlin_prime(weighted_sum)
+    # with different batch we have different weighted sum
+    self.weighted_sum_inputs = {}
+
+
+  def forward_to_upper(self, batch_id, forward_matrix, forward_labels, istrain):
     """
     forward output to upper layer
     """
@@ -44,11 +69,27 @@ class Layer():
                        is_train=istrain))
     print("get response form upper layer", res.message)
 
-  def backward_lower(self, batch_id, delta, labels):
+
+  def backward_to_lower(self, batch_id, partial_delta, labels):
     """
-    back propagate error delta to lower layer
+    back propagate error partial_delta to lower layer
+    partial_delta = dot(self.weights.T, self.delta)
+    self.delta = delta_received_from_upper * nonlin_prime(z)
     """
-    # TODO
+    # create stub for lower layer
+    if not self.lower_layer_stub:
+      self.create_lower_stub()
+
+    # convert partial_delta matrix to bytes string
+    bytes_delta = pkl.dumps(partial_delta)
+    bytes_labels = pkl.dumps(labels)
+
+    res = self.lower_layer_stub.UpdateDelta(
+      nn_pb.BackwardMsg(batch_id=batch_id,
+                        delta_matrix=bytes_delta,
+                        labels=bytes_labels))
+    print("get response from lower layer", res.message)
+
 
   def create_upper_stub(self):
     """ create upper_layer_stub for exchanging data between grpc"""
@@ -66,6 +107,63 @@ class Layer():
       self.lower_layer_stub = nn_pb_grpc.LayerDataExchangeStub(channel)
     else:
       print("no lower layer has been specified")
+
+
+  def init_weights(self, load_weights):
+    """
+    if load_weights is specified load the trained weights
+    """
+    if load_weights:
+      # TODO 
+      pass
+    else:
+      # x: lower layer nodes n
+      # y: current layer nodes n
+      x = self.weights_shape[1]
+      y = self.weights_shape[0]
+      self.weights = np.random.randn(y, x) / np.sqrt(x) # pylint: disable=no-member 
+
+
+  # implementing rpc services
+  def UpdateInput(self, request, context):
+    """ Invoked by lower layer
+    Once inputs updated, start computing the weighted sum
+    then activation outputs,
+    then forward outputs to next layer
+    request: ForwardMsg
+    """
+    if not self.weights:
+      print("Weights of {} have not initialized".format(self.layer_name))
+      import sys
+      sys.exit(-1)
+
+    # get values from message
+    batch_id = request.batch_id
+    bytes_outputs_of_lower = request.output_matrix
+    bytes_labels = request.labels
+    is_train = request.is_train
+    # bytes to numpy array
+    outputs_of_lower = pkl.loads(bytes_outputs_of_lower)
+    labels = pkl.loads(bytes_labels)
+
+    # saving inputs
+    inputs = {'matrix': outputs_of_lower,
+              'labels': labels}
+    self.lower_layer_outputs[batch_id] = inputs
+    weighted_sum = np.dot(outputs_of_lower, self.weights)
+    self.weighted_sum_inputs[batch_id] = weighted_sum
+
+    # forward layer outputs
+    activations = self.nonlin(weighted_sum) # apply element wise
+    self.forward_to_upper(batch_id, activations, labels, is_train)
+
+
+  def UpdateDelta(self, request, context):
+    """ Invoked by upper layer
+    TODO
+    """
+
+
 
 
 
