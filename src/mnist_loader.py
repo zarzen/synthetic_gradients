1cb283b01c454b3d822154b113f4e53a2c6864c0
Zhen Zhang <zhangzhen.dev@gmail.com>
2017-12-06 16:15:07 -0500

    modified data loader for python3

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -4,17 +4,18 @@ convert labels to one hot representation
 
 import pickle as pkl
 import numpy as np
+import gzip
 
 
 def load_data(data_path):
   """ load mnist data, and convert labels to one hot representation"""
-  with open(data_path, 'rb') as f:
-    train, val, test = pkl.load(f)
+  with gzip.open(data_path, 'rb') as f:
+    train, val, test = pkl.load(f, encoding='latin1')
 
   # transform labels in training data to one hot vector
   train_y = train[1]
   n = len(train_y)
   one_hot_y = np.zeros((n, 10))
   one_hot_y[np.arange(n), train_y] = 1
-  train[1] = one_hot_y
-  return train, val, test
+  # train[1] = one_hot_y
+  return (train[0], one_hot_y), val, test
